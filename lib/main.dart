20dd4ea531ff64bbbba2396589b1cea7d3de49de
Foddie <40058903+Foddie2@users.noreply.github.com>
2021-02-12 17:25:28 +0300

    Created a flutter App and Integrated it with Daraja Mpesa API
    Added iput fields for Amount and Phone number

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mpesa_flutter_plugin/mpesa_flutter_plugin.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
 
 void main() {
@@ -15,17 +16,26 @@ class DarajaFlutter extends StatefulWidget {
 }
 
 class _DarajaFlutterState extends State<DarajaFlutter> {
+  final TextEditingController phonenumberController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
   //create the lipaNaMpesa method here.Please note, the method can have any name, I chose lipaNaMpesa
 
   Future<void> lipaNaMpesa() async {
+    var phoneNumber = phonenumberController.text;
+    var CodeKe = '254';
+    phoneNumber = phoneNumber.substring(1);
+    phoneNumber = CodeKe+phoneNumber;
+    print(phonenumberController.text);
+    dynamic amount = double.parse(amountController.text);
+
     dynamic transactionInitialisation;
     try {
       transactionInitialisation =
           await MpesaFlutterPlugin.initializeMpesaSTKPush(
               businessShortCode: "174379",
               transactionType: TransactionType.CustomerPayBillOnline,
-              amount: 1.0,
-              partyA: "254711974716",
+              amount: amount,
+              partyA: phoneNumber,
               partyB: "174379",
 //Lipa na Mpesa Online ShortCode
               callBackURL: Uri(scheme: "http",
@@ -33,7 +43,7 @@ class _DarajaFlutterState extends State<DarajaFlutter> {
                   path: "/15hdjxp1"),
 //This url has been generated from http://mpesa-requestbin.herokuapp.com/?ref=hackernoon.com for test purposes
               accountReference: "Foddie",
-              phoneNumber: "254711974716",
+              phoneNumber: phoneNumber,
               baseUri: Uri(scheme: "https",
                   host: "sandbox.safaricom.co.ke"),
               transactionDesc: "purchase",
@@ -54,22 +64,44 @@ class _DarajaFlutterState extends State<DarajaFlutter> {
       theme: ThemeData(primaryColor: Colors.green),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mpesa Payment Demo'),
+          title: Text('Mpesa Payment by Foddie'),
           centerTitle: true,
         ),
         body: Center(
-          child: RaisedButton(
-            color: Colors.green,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            onPressed: () {
-              lipaNaMpesa();
-            },
-            child: Text(
-              "Lipa na Mpesa",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          child: Container(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: phonenumberController,
+                    decoration: InputDecoration(labelText: "Enter your number"),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ], // Only numbers can be entered
+                  ),
+                  TextFormField(
+                    controller: amountController,
+                    decoration: InputDecoration(labelText: "Amount"),
+                    keyboardType: TextInputType.number, // Only numbers can be entered
+                  ),
+                  SizedBox(height: 30,),
+                  RaisedButton(
+                    color: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    onPressed: () {
+                      lipaNaMpesa();
+                    },
+                    child: Text(
+                      "Lipa na Mpesa",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+          )
         ),
       ),
     );
